4db08cb700d2dc494cd51956739a2927d00bae5e
Shivam Ramraika <shivamramraikaweb.dev@gmail.com>
2026-02-27 19:25:43 +0000

    Try fixing this error: `Runtime TypeError: Failed to construct 'Image':

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,9 @@
+
 "use client";
 
 import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { 
